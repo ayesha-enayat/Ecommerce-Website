134505a592118bc583a229c063f27ac41160e7bd
ayeshaenayat <ayeshaenayat123enayat@gmail.com>
2025-04-14 10:03:31 +0500

    create pages and routing

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Collection from './pages/Collection';
+import PlaceOrder from './pages/PlaceOrder';
+
+const App = () => {
+  return (
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/collection' element={<Collection />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/product/:productId' element={<Product />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/place-order' element={<PlaceOrder />} />
+        <Route path='/orders' element={<Orders />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App;

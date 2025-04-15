13d47fb8cec5459226cc034932f87d448829cc04
ayeshaenayat <ayeshaenayat123enayat@gmail.com>
2025-04-15 16:18:18 +0500

    collection page complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,10 +12,12 @@ import Orders from './pages/Orders';
 import Contact from './pages/Contact';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Searchbar from './context/Searchbar';
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
       <Navbar/>
+      <Searchbar/>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/collection' element={<Collection />} />
--- frontend/src/context/context.jsx
+++ frontend/src/context/context.jsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { products } from "../assets/frontend_assets/assets";
 
 export const ShopContext=createContext();
@@ -6,11 +6,17 @@ const ShopContextProvider=(props)=>{
 
     const currency= '$';
     const deliveryFee=10;
+    const[search,setSearch]=useState('');
+    const[showSearch,setShowSearch]=useState(false)
 
     const value={
         products,
         currency,
-        deliveryFee
+        deliveryFee,
+        search,
+        setSearch,
+        showSearch,
+        setShowSearch
     }
 
     return(

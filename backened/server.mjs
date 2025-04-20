e6194bb26259645ab570859f312885f21cb7c028
ayeshaenayat <ayeshaenayat123enayat@gmail.com>
2025-04-20 23:49:19 +0500

    mongodb connection

---
+++ backened/server.mjs
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from "dotenv";
+import connectDB from './config/mongodb.mjs';
+
+// App config
+dotenv.config();
+const app = express();  
+const port = process.env.PORT || 3007;
+connectDB();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+// API endpoint
+app.get('/', (req, res) => {
+  res.send('API Working');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -21,7 +21,7 @@ const Contact = () => {
         </div>
 
       </div>
-      <NewsLetterBox/>
+      <NewsLetterBox />
     </div>
   )
 }

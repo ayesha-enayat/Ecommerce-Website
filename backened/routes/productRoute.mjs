dca0c9b5a0d761d939c4d49c3e793cf8081259ff
ayeshaenayat <ayeshaenayat123enayat@gmail.com>
2025-04-24 15:57:07 +0500

    create auth for admin

--- backened/routes/productRoute.mjs
+++ backened/routes/productRoute.mjs
@@ -1,10 +1,11 @@
 import express from 'express'
 import { addProduct, listProducts, removeProduct, singleProduct } from '../controllers/productController.mjs'
 import upload from '../middleware/multer.mjs';
+import adminAuth from '../middleware/adminAuth.mjs';
 
 
 const productRouter = express.Router();
-productRouter.post('/add',
+productRouter.post('/add', adminAuth,
     upload.fields([
         { name: 'image1', maxCount: 1 },
         { name: 'image2', maxCount: 1 },
@@ -14,7 +15,7 @@ productRouter.post('/add',
     ,
     addProduct
 );
-productRouter.delete('/remove', removeProduct);
+productRouter.delete('/remove', adminAuth, removeProduct);
 productRouter.get('/single', singleProduct);
 productRouter.get('/list', listProducts);
 

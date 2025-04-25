80f18d06ebcebbcd2a7546f8de95dcf505dce6af
ayeshaenayat <ayeshaenayat123enayat@gmail.com>
2025-04-25 13:18:23 +0500

    create admin dashboard

---
+++ admin/src/components/Login.jsx
@@ -0,0 +1,49 @@
+import axios from 'axios';
+import React from 'react'
+import { useState } from 'react'
+import { backendUrl } from '../App.jsx';
+import { toast } from 'react-toastify';
+
+const Login = ({ setToken }) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async (e) => {
+        try {
+            e.preventDefault();
+            console.log("Backend URL:", backendUrl);
+
+            const response = await axios.post(backendUrl + '/api/user/admin', { email, password });
+            console.log(response);
+            if (response.data.success) {
+                setToken(response.data.token);
+            } else {
+                toast.error(response.data.message)
+            }
+        }
+        catch (err) {
+            console.log(err.message);
+            toast.error(err.message)
+        }
+    }
+    return (
+        <div className='flex items-center justify-center min-h-screen w-full'>
+            <div className='bg-white shadow-md rounde-lg px-8 py-6 max-w-md'>
+                <h1 className='tet-2xl font-bold mb-4'>Admin Panel</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className='mb-3 min-w-72'>
+                        <p className='text-sm font-medium text-gray-700 mb-2'>Email Address</p>
+                        <input onChange={(e) => setEmail(e.target.value)} value={email} className="rounded-md w-full px-3 py-2 border border-gray-30 outline-none" type="email" placeholder='Enter your email' required />
+                    </div>
+                    <div className='mb-3 min-w-72'>
+                        <p className='text-sm font-medium text-gray-700 mb-2'>Password</p>
+                        <input onChange={(e) => setPassword(e.target.value)} value={password} className="rounded-md w-full px-3 py-2 border border-gray-30 outline-none" type="password" placeholder='Enter your password' required />
+                    </div>
+                    <button className="mt-2 w-full py-2 px-4 rounded-md text-white bg-black" type='submit'>Login</button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Login
